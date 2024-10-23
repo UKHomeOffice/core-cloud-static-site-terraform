f02b53847f6c5682eccfd7384fc4d4ae43a1ac21
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2024-10-23 10:03:03 +0100

    remove provider from WAF resource, as inherited from parent module

--- WAF.tf
+++ WAF.tf
@@ -2,7 +2,6 @@ resource "aws_wafv2_web_acl" "default" {
   name        = "cc-static-site-${var.tenant_vars.product}-${var.tenant_vars.component}"
   description = "Static Site WAF rule for ${var.tenant_vars.product} ${var.tenant_vars.component}"
   scope       = "CLOUDFRONT"
-  provider    = aws.us-east-1
 
   tags = local.common_tags
 

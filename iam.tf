a9a6131e02bbfedcd073d2a5dc594cf018183018
Becky <93141344+beckywhitemartin@users.noreply.github.com>
2024-10-28 16:12:48 +0000

    add further variablisation into resource names, to prevent duplication when module is run in a for loop (#4)

--- iam.tf
+++ iam.tf
@@ -38,7 +38,7 @@ resource "aws_iam_role_policy_attachment" "static_site_policy_attachment" {
 }
 
 resource "aws_iam_policy" "static_site_policy" {
-  name   = "static-site-iam-policy"
+  name   = "static-site-${var.tenant_vars.component}-iam-policy"
   policy = data.aws_iam_policy_document.static_site_policy_document.json
 }
 

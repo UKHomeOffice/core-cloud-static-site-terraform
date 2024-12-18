447915613063c13b9d85659559a472b753df7d6d
Iqbal Shaikh <iqbal.shaikh2@digital.homeoffice.gov.uk>
2024-12-18 13:56:16 +0000

    CCL-1542 - Adding common WAF rule from AWS

--- WAF.tf
+++ WAF.tf
@@ -10,10 +10,33 @@ resource "aws_wafv2_web_acl" "default" {
     allow {}
   }
 
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      count {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "static-site-${var.tenant_vars.product}-${var.tenant_vars.component}"
+      sampled_requests_enabled   = false
+    }
+  }
+  
   visibility_config {
     cloudwatch_metrics_enabled = false
     metric_name                = "static-site-${var.tenant_vars.product}-${var.tenant_vars.component}"
     sampled_requests_enabled   = false
   }
+
 
 }

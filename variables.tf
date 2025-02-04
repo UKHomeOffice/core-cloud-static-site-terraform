2d91234b08592b61a825e4ee6384a519d3ecfef8
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2025-02-04 14:36:14 +0000

    define variables within tenant_vars object

--- variables.tf
+++ variables.tf
@@ -1,5 +1,18 @@
 variable "tenant_vars" {
-  type = any
+  type = object({
+    # enforced tags
+    cost_centre                     = string
+    account_code                    = string
+    portfolio_id                    = string
+    project_id                      = string
+    service_id                      = string
+    # other attributes
+    repository                      = string
+    github_environment_name         = string
+    cloudfront_aliases              = string
+    cloudfront_cert                 = string
+    cloudfront_function_rewrite_arn = string
+  })
 }
 
 variable "cloud_front_default_vars" {

87368f1130f462275acd3ad743becbde2fd1a86f
Rebecca Whitehurst-Martin <Rebecca.Whitehurst-Martin@digital.homeoffice.gov.uk>
2025-02-04 16:09:07 +0000

    modify tag vars to remove nullable

--- variables.tf
+++ variables.tf
@@ -1,12 +1,13 @@
 variable "tenant_vars" {
   type = object({
-    # enforced tags
-    cost_centre                     = object({type=string, nullable=false})
-    account_code                    = object({type=string, nullable=false})
-    portfolio_id                    = object({type=string, nullable=false})
-    project_id                      = object({type=string, nullable=false})
-    service_id                      = object({type=string, nullable=false})
-    # other attributes
+    # enforced tags, all required attributes for costing and ID
+    cost_centre                     = string
+    account_code                    = string
+    portfolio_id                    = string
+    project_id                      = string
+    service_id                      = string
+
+    # other attributes, required for cloudfront creation
     repository                      = string
     github_environment_name         = string
     cloudfront_aliases              = string
